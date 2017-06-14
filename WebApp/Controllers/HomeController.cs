8144d94903c37efddfd0ec0845c9b69166198ab5
Sevil Turner <sevilturner@yahoo.com>
2017-06-13 22:05:31 -0700

    initial commit

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Notes.WebApp.Models;
+
+namespace Notes.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = new Data()
+            {
+                Message = "Hello from Meetup",
+            };
+
+            data.Message = "Another message";
+
+            return View(data);
+        }
+    }
+}
