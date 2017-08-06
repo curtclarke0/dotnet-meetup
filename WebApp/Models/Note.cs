6c7d28438de95a2c19f71453826031dd8afb2dfc
Sevil Turner <sevilturner@hotmail.com>
2017-08-06 16:57:29 -0700

    adding an Id property to model

--- WebApp/Models/Note.cs
+++ WebApp/Models/Note.cs
@@ -4,6 +4,7 @@ namespace Notes.WebApp.Models
 {
     public class Note
     {
+        public Guid Id { get; set; }
         public string Message { get; set; }
         public string Title { get; set; }
         public string Summary { get; set; }
