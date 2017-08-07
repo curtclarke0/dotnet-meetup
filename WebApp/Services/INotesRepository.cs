5085207daa65b22a68af8144ed2c17e3130b7c83
Sevil Turner <sevilturner@hotmail.com>
2017-08-06 18:21:04 -0700

    define an interface for working with Notes

---
+++ WebApp/Services/INotesRepository.cs
@@ -0,0 +1,17 @@
+﻿using Notes.WebApp.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Notes.WebApp.Services
+{
+    public interface INotesRepository
+    {
+        void Load();
+        void Save();
+
+        IEnumerable<Note> GetAll();
+        Note Get(Guid id);
+        Note AddOrUpdate(Note note);
+        void Delete(Note note);
+    }
+}
